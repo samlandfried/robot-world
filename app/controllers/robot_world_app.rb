b7e29a6e33098889be4a5f879fb98e2820ffe27c
Sam Landfried <samlandfried@gmail.com>
2017-03-14 15:24:37 -0600

    Add edit button to robots list

--- app/controllers/robot_world_app.rb
+++ app/controllers/robot_world_app.rb
@@ -5,10 +5,6 @@ require './app/models/robot.rb'
 class RobotWorldApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
 
-  get '/' do
-    'hello world'
-  end
-
   get '/robots' do
     @robots = Robot.all
     erb :index
--- app/models/robot.rb
+++ app/models/robot.rb
@@ -3,13 +3,14 @@ require 'sqlite3'
 
 class Robot
 
-  attr_reader :name, :city, :state, :department
+  attr_reader :id, :name, :city, :state, :department
 
   def initialize(robot_params)
     @name = robot_params['name']
     @city = robot_params['city']
     @state = robot_params['state']
     @department = robot_params['department']
+    @id = robot_params['id'] if robot_params['id']
     @database = SQLite3::Database.new('db/robot_world_development.db')
     @database.results_as_hash = true
   end
