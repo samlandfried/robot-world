587364caff437249ded959177ce50bf29cfea0bc
Sam Landfried <samlandfried@gmail.com>
2017-03-14 14:20:33 -0600

    Init

--- app/controllers/robot_world_app.rb
+++ app/controllers/robot_world_app.rb
@@ -1,10 +1,20 @@
-require_relative '../models/robot.rb'
+require 'sinatra'
+
+require './app/models/robot.rb'
 
 class RobotWorldApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
 
+  get '/' do
+    'hello world'
+  end
+
   get '/robots' do
     erb :index
+  end
+
+  get '/robots/new' do
+    erb :new
   end
 end
 
